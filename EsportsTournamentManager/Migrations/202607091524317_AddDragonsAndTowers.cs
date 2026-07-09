9b1d67a01fac59c52f363de86483ee5a59819c4a
ttung205 <23010044@st.phenikaa-uni.edu.vn>
2026-07-09 22:27:37 +0700

    feat: add migrations and schema support for dragons and towers entities

---
+++ EsportsTournamentManager/Migrations/202607091524317_AddDragonsAndTowers.cs
@@ -0,0 +1,32 @@
+﻿namespace EsportsTournamentManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDragonsAndTowers : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MatchMaps", "Team1DragonsKilled", c => c.Int(nullable: false));
+            AddColumn("dbo.MatchMaps", "Team2DragonsKilled", c => c.Int(nullable: false));
+            AddColumn("dbo.MatchMaps", "Team1TowersDestroyed", c => c.Int(nullable: false));
+            AddColumn("dbo.MatchMaps", "Team2TowersDestroyed", c => c.Int(nullable: false));
+            AddColumn("dbo.Players", "Nationality", c => c.String());
+            AddColumn("dbo.Players", "DateOfBirth", c => c.DateTime());
+            AddColumn("dbo.Players", "ImagePath", c => c.String());
+            AddColumn("dbo.Teams", "Region", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Teams", "Region");
+            DropColumn("dbo.Players", "ImagePath");
+            DropColumn("dbo.Players", "DateOfBirth");
+            DropColumn("dbo.Players", "Nationality");
+            DropColumn("dbo.MatchMaps", "Team2TowersDestroyed");
+            DropColumn("dbo.MatchMaps", "Team1TowersDestroyed");
+            DropColumn("dbo.MatchMaps", "Team2DragonsKilled");
+            DropColumn("dbo.MatchMaps", "Team1DragonsKilled");
+        }
+    }
+}
